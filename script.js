92079e641bfe7c5d7256430b00a0506e346ea38b
ultimate-knight <maaz2607@gmail.com>
2023-08-21 19:51:23 +0530

    Add 'script' directory to repository

---
+++ script.js
@@ -0,0 +1,22 @@
+let message
+let user='john',age=25,student='engineering'
+let know
+let $=13;
+let _=9;
+const nar='13.0';
+const color_red="#F00";
+let color=color_red;
+message='Hello my name is maaz';
+message="i don't know";
+know=message
+saved=message;
+alert(message);
+alert(saved);
+alert(user);
+alert(age);
+alert(student);
+alert($+_);
+alert(nar);
+alert(pageLoadTime);
+alert(color);
+//let,class,return and function cannot be used as variables.//
